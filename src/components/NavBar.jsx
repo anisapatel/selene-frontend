46b76a4373186585be3cf73875ac646069b5b3e8
anisapatel <anisa_patel@live.co.uk>
2020-11-17 17:45:46 +0000

    removed auth0

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,9 @@
 import React, { Component } from "react";
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Link } from "react-router-dom";
-// import { connect } from "react-redux";
 import "./NavBar.css";
 
 class NavBar extends Component {
-  //Define your linkFunc so it will redirect to specified route provided via argument.
-
-  // linkFunc(path) {
-  //   this.props.history.push(path);
-  // }
-
   render() {
     return (
       <nav>
@@ -30,14 +23,5 @@ class NavBar extends Component {
     );
   }
 }
-
-// const mapStateToProps = (state) => {
-//   return {
-//     user: this.state.user,
-//   };
-// };
-
-// //Then wrap our Component with the HOC, and the connect double invoked.
-// export default withRouter(connect(mapStateToProps)(NavBar));
 
 export default NavBar;
